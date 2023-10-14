173b95d60a33280a591f8c19a2337f56df471a75
Michel <castellanosm@utb.edu.co>
2023-10-14 11:12:25 -0500

    MS-mantenimiento

--- app/MS-mantenimiento/app.py
+++ app/MS-mantenimiento/app.py
@@ -1,12 +1,30 @@
+from flask import Flask, render_template, jsonify, request
 import json
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def mantenimiento():
-    return render_template('mantenimiento.html')
+# Leer datos de vehículos desde un archivo JSON
+with open('data/buses.json', 'r') as vehiculos_file:
+    vehiculos = json.load(vehiculos_file)
 
+# Ruta para cargar la página HTML
+@app.route('/')
+def mostrar_tablas():
+    return render_template('mantenimiento.html', datos=vehiculos)
+
+# En la ruta para cambiar el estado de un vehículo
+@app.route('/cambiar_estado/<placa>', methods=['POST'])
+def cambiar_estado(placa):
+    nuevo_estado = request.json['nuevo_estado']
+
+    for vehiculo in vehiculos:
+        if vehiculo['placa'] == placa:
+            vehiculo['estado'] = nuevo_estado
+
+    with open('buses.json', 'w') as vehiculos_file:
+        json.dump(vehiculos, vehiculos_file, indent=4)
+
+    return jsonify({'nuevo_estado': nuevo_estado})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
