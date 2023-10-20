ecf7a5aa6bad20a26a1091188f2e792981d61fd5
BRNDLD <vegab@utb.edu.co>
2023-10-20 18:15:33 -0500

    port

--- MS-mantenimiento/MSmantenimiento.py
+++ MS-mantenimiento/MSmantenimiento.py
@@ -81,4 +81,4 @@ async def agregar_vehiculo(
     return RedirectResponse(url='/')
 
 if __name__ == '__main__':
-    uvicorn.run(mantenimiento, host="127.0.0.1", port=8000)
+    uvicorn.run(mantenimiento, host="127.0.0.1", port=8002)
