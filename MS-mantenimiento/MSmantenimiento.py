4235553b5b68dba724f4c11a5ce658b7e306dfa9
Michel <castellanosm@utb.edu.co>
2023-11-18 21:41:39 -0500

    ...

--- MS-mantenimiento/MSmantenimiento.py
+++ MS-mantenimiento/MSmantenimiento.py
@@ -1,3 +1,4 @@
+import ssl
 from fastapi import FastAPI, HTTPException, Form, Request
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
@@ -15,6 +16,11 @@ client = MongoClient("mongodb+srv://publictransit:qwerty32@pta.bueovsa.mongodb.n
 db = client["Vehiculos"]
 collection = db["vehiculos"]
 
+cluster = MongoClient(
+    "url",
+    ssl=True,
+    ssl_cert_reqs=ssl.CERT_NONE,
+)
 
 # Configurar la carpeta de plantillas para Jinja2
 templates = Jinja2Templates(directory="MS-mantenimiento/templates")
